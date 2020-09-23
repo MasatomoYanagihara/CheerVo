a61e264b0af8651ffb00436819b70eed5963b2bf
Yana <masatomo.yanagihara.jp@gmail.com>
2020-09-23 17:37:37 +0900

    ログイン・会員登録画面作成

--- resources/js/pages/Login.vue
+++ resources/js/pages/Login.vue
@@ -1,3 +1,44 @@
 <template>
-  <h1>Login</h1>
+    <div>
+        <v-card width="400px" class="mx-auto mt-8">
+            <v-card-title>
+                <h1 class="display-1">ログイン</h1>
+            </v-card-title>
+            <v-card-text>
+                <v-form @submit.prevent="login">
+                    <v-text-field
+                        prepend-icon="mdi-account-circle"
+                        label="E-mail"
+                        v-model="loginForm.email"
+                    />
+                    <v-text-field
+                        prepend-icon="mdi-lock"
+                        type="password"
+                        label="Password"
+                        v-model="loginForm.password"
+                    />
+                    <v-card-actions>
+                        <v-btn type="submit">ログイン</v-btn>
+                    </v-card-actions>
+                </v-form>
+            </v-card-text>
+        </v-card>
+    </div>
 </template>
+<script>
+export default {
+    data() {
+        return {
+            loginForm: {
+                email: "",
+                password: ""
+            }
+        };
+    },
+    methods: {
+        login() {
+            console.log(this.loginForm);
+        }
+    }
+};
+</script>
---
+++ resources/js/pages/Register.vue
@@ -0,0 +1,57 @@
+<template>
+    <div>
+        <v-card width="400px" class="mx-auto mt-8">
+            <v-card-title>
+                <h1 class="display-1">会員登録</h1>
+            </v-card-title>
+            <v-card-text>
+                <v-form @submit.prevent="register">
+                    <v-text-field
+                        prepend-icon="mdi-account-circle"
+                        label="Name"
+                        v-model="registerForm.name"
+                    />
+                    <v-text-field
+                        prepend-icon="mdi-account-circle"
+                        label="E-mail"
+                        v-model="registerForm.email"
+                    />
+                    <v-text-field
+                        prepend-icon="mdi-lock"
+                        type="password"
+                        label="Password"
+                        v-model="registerForm.password"
+                    />
+                    <v-text-field
+                        prepend-icon="mdi-lock"
+                        type="password"
+                        label="Password (confirm)"
+                        v-model="registerForm.password_confirmation"
+                    />
+                    <v-card-actions>
+                        <v-btn type="submit">登録する</v-btn>
+                    </v-card-actions>
+                </v-form>
+            </v-card-text>
+        </v-card>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            registerForm: {
+                name: "",
+                email: "",
+                password: "",
+                password_confirmation: ""
+            }
+        };
+    },
+    methods: {
+        register() {
+            console.log(this.registerForm);
+        }
+    }
+};
+</script>
