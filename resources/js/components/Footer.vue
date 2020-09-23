81b65cb8521e362260d90f66944a4ab136b37831
Yana <masatomo.yanagihara.jp@gmail.com>
2020-09-23 17:37:12 +0900

    ヘッダー、フッター追加

---
+++ resources/js/components/Footer.vue
@@ -0,0 +1,7 @@
+<template>
+    <v-footer absolute class="font-weight-medium">
+        <v-col class="text-center" cols="12">
+            <strong>Voice</strong>
+        </v-col>
+    </v-footer>
+</template>
---
+++ resources/js/components/Navbar.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <v-app-bar color="white" dense>
+            <v-toolbar-title>Page title</v-toolbar-title>
+
+            <v-spacer></v-spacer>
+
+            <v-btn outlined small>
+                ログイン
+            </v-btn>
+            <v-btn outlined small class="register_btn">
+                会員登録
+            </v-btn>
+        </v-app-bar>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.register_btn {
+  margin-left: 2%;
+}
+</style>
