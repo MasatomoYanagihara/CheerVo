bb468a0e960233396106483ca8528f23e5870c92
Yana <masatomo.yanagihara.jp@gmail.com>
2020-09-23 17:36:23 +0900

    Vuetify追加

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,19 @@
-// import "./bootstrap";
+require("./bootstrap");
+
 import Vue from "vue";
 import router from "./router";
 import App from "./App.vue";
+import Vuetify from "vuetify";
+import "vuetify/dist/vuetify.min.css";
+import "@mdi/font/css/materialdesignicons.css";
+import "material-design-icons-iconfont/dist/material-design-icons.css";
+
+Vue.use(Vuetify);
 
 new Vue({
     el: "#app",
     router,
+    vuetify: new Vuetify(),
     components: { App },
     template: "<App />"
 });
