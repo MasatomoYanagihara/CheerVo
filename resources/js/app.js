861a0bc974b147acbbceca1c711dc2ec54158cdf
Yana <masatomo.yanagihara.jp@gmail.com>
2020-09-24 10:38:29 +0900

    Add:アイコンファイル

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,18 +1,19 @@
-require("./bootstrap");
-
+import './bootstrap'
 import Vue from "vue";
 import router from "./router";
+import store from "./store";
 import App from "./App.vue";
 import Vuetify from "vuetify";
 import "vuetify/dist/vuetify.min.css";
-import "@mdi/font/css/materialdesignicons.css";
-import "material-design-icons-iconfont/dist/material-design-icons.css";
+import "@mdi/font/css/materialdesignicons.css"; // アイコン
+import "material-design-icons-iconfont/dist/material-design-icons.css"; // アイコン
 
 Vue.use(Vuetify);
 
 new Vue({
     el: "#app",
     router,
+    store,
     vuetify: new Vuetify(),
     components: { App },
     template: "<App />"
