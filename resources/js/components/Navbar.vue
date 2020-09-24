4f085379f2c9ade262e56fd76e4d0c1aa81f19a9
Yana <masatomo.yanagihara.jp@gmail.com>
2020-09-24 13:11:21 +0900

    Add:ログイン中かどうかで表示切り替え

--- resources/js/components/Navbar.vue
+++ resources/js/components/Navbar.vue
@@ -9,30 +9,45 @@
 
             <v-spacer></v-spacer>
 
-            <Router-link :to="{ name: 'login' }">
-                <v-btn outlined small>
-                    ログイン
-                </v-btn>
-            </Router-link>
-            <Router-link :to="{ name: 'logout' }">
+            <div v-if="isLogin">
+                <div>
+                    {{ username }}
+                </div>
                 <v-btn outlined small @click="logout">
                     ログアウト
                 </v-btn>
-            </Router-link>
-            <Router-link :to="{ name: 'register' }">
-                <v-btn outlined small class="register_btn">
-                    会員登録
-                </v-btn>
-            </Router-link>
+            </div>
+            <div v-else>
+                <Router-link :to="{ name: 'login' }">
+                    <v-btn outlined small>
+                        ログイン
+                    </v-btn>
+                </Router-link>
+                <Router-link :to="{ name: 'register' }">
+                    <v-btn outlined small class="register_btn">
+                        会員登録
+                    </v-btn>
+                </Router-link>
+            </div>
         </v-app-bar>
     </div>
 </template>
 <script>
 export default {
+    computed: {
+        // ログインチェック
+        isLogin() {
+            return this.$store.getters["auth/check"];
+        },
+        // ユーザーネーム取得
+        username() {
+            return this.$store.getters["auth/username"];
+        }
+    },
     methods: {
         async logout() {
             await this.$store.dispatch("auth/logout");
-            console.log('ログアウトしました')
+            console.log("ログアウトしました");
 
             this.$router.push("/login");
         }
