67e359454bf3e8f50a64407518a0cc9e5c5b011d
Yana <masatomo.yanagihara.jp@gmail.com>
2020-11-08 15:49:10 +0900

    Fix: Seeder追加

--- database/factories/VoiceFactory.php
+++ database/factories/VoiceFactory.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Str;
 $factory->define(App\Voice::class, function (Faker $faker) {
     return [
         'id' => Str::random(12),
-        'user_id' => $faker->numberBetween(1,3),
+        'user_id' => $faker->numberBetween(1,6),
         'title' => $faker->realText($faker->numberBetween(10,20)),
         'filename' => Str::random(12) . '.mp3',
         'created_at' => $faker->dateTime(),
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -20,8 +20,8 @@ class UsersTableSeeder extends Seeder
         ]);
         DB::table('users')->insert(
             [
-                'name' => '原口菜々',
-                'email' => 'haraguchi@hoge.jp',
+                'name' => 'NANA',
+                'email' => 'nana@hoge.jp',
                 'img_url' => 'https://images.unsplash.com/photo-1596797882870-8c33deeac224?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
                 'password' => Hash::make('hogehoge'),
         ]);
@@ -34,9 +34,30 @@ class UsersTableSeeder extends Seeder
             ]);
         DB::table('users')->insert(
             [
+                'name' => 'ハリー',
+                'email' => 'hari@hoge.jp',
+                'img_url' => 'https://images.unsplash.com/photo-1569079349972-cbf332db1153?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+                'password' => Hash::make('hogehoge'),
+            ]);
+        DB::table('users')->insert(
+            [
+                'name' => 'Mike',
+                'email' => 'mike@hoge.jp',
+                'img_url' => 'https://images.unsplash.com/photo-1603072007571-7295a309e45f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+                'password' => Hash::make('hogehoge'),
+            ]);
+        DB::table('users')->insert(
+            [
+                'name' => 'Lisa',
+                'email' => 'lisa@hoge.jp',
+                'img_url' => 'https://images.unsplash.com/photo-1603471567917-74fa6b3f7fe4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+                'password' => Hash::make('hogehoge'),
+            ]);
+        DB::table('users')->insert(
+            [
                 'name' => 'testuser',
                 'email' => 'test@test.jp',
-                'img_url' => 'https://images.unsplash.com/photo-1593626986521-f24d25d60103?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+                'img_url' => 'https://images.unsplash.com/photo-1587354004405-bbce24c893c8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
                 'password' => Hash::make('testuser'),
             ]);
     }
